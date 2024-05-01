23185920173e661ee92056d270be233785baa6cb
Abel Salgado Romero <abelromero@gmail.com>
2024-05-01 20:38:02 +0200

    Increase size to improve usability

--- src/conf.lua
+++ src/conf.lua
@@ -9,6 +9,7 @@ resolutions = {}
 resolutions['720p-square'] = Resolution(720, 720)
 resolutions['720p'] = Resolution(1280, 720)
 resolutions['1080p'] = Resolution(1920, 1080)
+resolutions['1080p-square'] = Resolution(1080, 1080)
 
 UI = {
   debug = true,
@@ -16,7 +17,7 @@ UI = {
 }
 
 function love.conf(t)
-  local defaultResolution = resolutions['720p-square']
+  local defaultResolution = resolutions['1080p-square']
   t.window.width = defaultResolution.width
   t.window.height = defaultResolution.height
 end
